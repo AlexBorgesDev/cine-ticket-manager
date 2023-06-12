2f09d5009e1bdaa0113a087d51efa610bfd73b68
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2023-06-12 19:46:16 -0300

    [skip ci] Creates AuthModule

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+
+import { AuthResolver } from './auth.resolver';
+import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [AuthResolver, AuthService, JwtStrategy, { provide: APP_GUARD, useClass: JwtAuthGuard }],
+})
+export class AuthModule {}
