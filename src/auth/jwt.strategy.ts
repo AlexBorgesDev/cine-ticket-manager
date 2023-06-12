ad96a3db9b72baeb60919c6b2f06aee7bd031ed4
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2023-06-12 19:45:38 -0300

    [skip ci] Creates JwtStrategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+import { MutationError } from '~/errors/errors.errors';
+import { User } from '~/user/user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any) {
+    const user = await User.findOneBy({ uuid: payload.sub });
+
+    if (!user) throw new MutationError('UNAUTHORIZED');
+    return user;
+  }
+}
