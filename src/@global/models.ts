cc2c7c0cd38e2fac412baa5e6c21ea456d4d780d
Alex Borges Ramos <43116288+AlexBorgesDev@users.noreply.github.com>
2023-06-13 20:04:39 -0300

    Creates custom BaseEntity (#15)
    * Creates custom BaseEntity
    
    * Creates custom BaseEntity

---
+++ src/@global/models.ts
@@ -0,0 +1,28 @@
+import { validateSync } from 'class-validator';
+import { BeforeInsert, BaseEntity as TypeOrmBaseEntity } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+import { MutationError } from '~/errors/errors.errors';
+
+export abstract class BaseEntity extends TypeOrmBaseEntity {
+  abstract id: number | string;
+
+  abstract uuid: string;
+
+  abstract createdAt: Date;
+
+  abstract updatedAt: Date;
+
+  @BeforeInsert()
+  protected onValidateBeforeInsert() {
+    if (!this.uuid) this.uuid = uuid();
+
+    const errors = validateSync(this);
+    if (errors.length > 0) {
+      throw new MutationError(
+        'INVALID_INPUT',
+        errors.map((e) => e.property),
+      );
+    }
+  }
+}
