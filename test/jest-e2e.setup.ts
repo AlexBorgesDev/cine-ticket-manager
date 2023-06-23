fbbbc54b086c0422629f835310fd43e4f0a3b436
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2023-06-23 17:48:59 -0300

    [skip ci] Updates e2e test setup

--- test/jest-e2e.setup.ts
+++ test/jest-e2e.setup.ts
@@ -1,1 +1,25 @@
-process.env.ENV_NAME = 'test';
+import { DataSource } from 'typeorm';
+
+import { EnvName } from '~/@global/types';
+import { databaseConfig } from '~/database/database.constants';
+
+let dataSource: DataSource;
+
+beforeAll(async () => {
+  process.env.ENV_NAME = EnvName.test;
+
+  dataSource = new DataSource({
+    ...databaseConfig(),
+    entities: ['src/**/*.entity.ts'],
+    migrations: ['src/database/migrations/*.ts'],
+    subscribers: ['src/**/*.subscriber.ts'],
+    synchronize: true,
+    migrationsRun: false,
+  });
+
+  if (!dataSource.isInitialized) await dataSource.initialize();
+});
+
+afterAll(async () => {
+  if (dataSource.isInitialized) await dataSource.destroy();
+});
--- test/jest-e2e.utils.ts
+++ test/jest-e2e.utils.ts
@@ -1,37 +1,17 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module, ValidationPipe } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER, APP_PIPE } from '@nestjs/core';
-import { GraphQLModule, Query, Resolver } from '@nestjs/graphql';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
 
 import { ENVs } from '~/@global/env.validation';
 import { AuthModule } from '~/auth/auth.module';
-import { databaseConfig } from '~/database/database.constants';
 import { HttpExceptionFilter } from '~/errors/errors.filters';
-
-@Resolver()
-class TestResolver {
-  @Query(() => String)
-  envName() {
-    return 'test';
-  }
-}
+import { UserModule } from '~/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, validate: ENVs.validate }),
-    TypeOrmModule.forRootAsync({
-      useFactory: (configService: ConfigService) => ({
-        ...databaseConfig(configService),
-        entities: ['src/**/*.entity.ts'],
-        migrations: ['src/database/migrations/*.ts'],
-        subscribers: ['src/**/*.subscriber.ts'],
-        dropSchema: true,
-        synchronize: true,
-      }),
-      inject: [ConfigService],
-    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
@@ -39,9 +19,9 @@ class TestResolver {
       sortSchema: true,
     }),
     AuthModule,
+    UserModule,
   ],
   providers: [
-    TestResolver,
     { provide: APP_PIPE, useClass: ValidationPipe },
     { provide: APP_FILTER, useClass: HttpExceptionFilter },
   ],
