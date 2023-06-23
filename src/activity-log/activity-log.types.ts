1a6860754799c996ec8866a97b5b11309668eadd
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2023-06-23 09:40:50 -0300

    [skip ci] Updates ActivityLog types

--- src/activity-log/activity-log.types.ts
+++ src/activity-log/activity-log.types.ts
@@ -1,1 +1,6 @@
-export enum ActivityLogAction {}
+export enum ActivityLogActionPrefix {
+  CREATES = 'CREATES_',
+  UPDATES = 'UPDATES_',
+  DELETES = 'DELETES_',
+  DELETES_SOFT = 'DELETES_SOFT_',
+}
