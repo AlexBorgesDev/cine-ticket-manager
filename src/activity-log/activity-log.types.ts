99c8423cde2bba19a30b7f04b93444b02f58fe6c
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2023-07-02 12:24:23 -0300

    [skip ci] Missing types and changes env.validation

--- src/activity-log/activity-log.types.ts
+++ src/activity-log/activity-log.types.ts
@@ -1,6 +1,13 @@
+import { EntityManager, ObjectLiteral } from 'typeorm';
+
 export enum ActivityLogActionPrefix {
   CREATES = 'CREATES_',
   UPDATES = 'UPDATES_',
   DELETES = 'DELETES_',
   DELETES_SOFT = 'DELETES_SOFT_',
 }
+
+export type DatabaseEvent<T> = { manager: EntityManager } & (
+  | { entity?: T | ObjectLiteral; databaseEntity: T }
+  | { entity: T | ObjectLiteral; databaseEntity?: T }
+);
